7192662069602d5d91a3c5305055a4fba9803c8e
lakshmivenkateswara2356 <lakshmivenkateswarapunnam@gmail.com>
2026-06-06 18:49:19 +0530

    updateslatest

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,45 +1,17 @@
-import { useState } from "react";
-import socialLinksData from "../data/socialLinks";
 import QRCodeCard from "../components/QRCodeCard";
 
 export default function Dashboard() {
-  const [links, setLinks] =
-    useState(socialLinksData);
-
-  const [platform, setPlatform] =
-    useState("");
-
-  const [url, setUrl] =
-    useState("");
-
-  const addLink = () => {
-    if (!platform || !url) return;
-
-    setLinks([
-      ...links,
-      {
-        id: Date.now(),
-        platform,
-        url,
-      },
-    ]);
-
-    setPlatform("");
-    setUrl("");
-  };
-
-  const deleteLink = (id) => {
-    setLinks(
-      links.filter(
-        (item) => item.id !== id
-      )
-    );
-  };
-
   return (
-    <div style={{ padding: 30 }}>
-     <QRCodeCard />
-      
+    <div
+      style={{
+        padding: "30px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <QRCodeCard />
     </div>
   );
 }
